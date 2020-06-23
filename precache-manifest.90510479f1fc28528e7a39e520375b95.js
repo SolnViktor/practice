482ff652d3036c7620b2b1b342e5d11a8cd009c8
SolnViktor <solnviktor@gmail.com>
2020-06-23 12:29:44 +0300

    Updates

---
+++ precache-manifest.90510479f1fc28528e7a39e520375b95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "799304a69d6917f826f1564270935ede",
+    "url": "/practice/index.html"
+  },
+  {
+    "revision": "75f014a714429c5dd1b7",
+    "url": "/practice/static/css/main.97585dd6.chunk.css"
+  },
+  {
+    "revision": "2dc9ce1d6803675e207d",
+    "url": "/practice/static/js/2.f35dea11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/practice/static/js/2.f35dea11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75f014a714429c5dd1b7",
+    "url": "/practice/static/js/main.d6051a8a.chunk.js"
+  },
+  {
+    "revision": "4c0223047dc23f83e817",
+    "url": "/practice/static/js/runtime-main.e92f5f07.js"
+  }
+]);
